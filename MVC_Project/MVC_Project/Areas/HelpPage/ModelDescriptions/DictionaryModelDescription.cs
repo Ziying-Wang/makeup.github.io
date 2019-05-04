d629fee86c7d4f580ba52f67770839957d433464
Kathleen Wang <kathleen@ou.edu>
2019-05-03 23:57:52 -0500

    project 1

---
+++ MVC_Project/MVC_Project/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MVC_Project.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MVC_Project/MVC_Project/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MVC_Project.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
