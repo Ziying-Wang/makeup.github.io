7acdc1d1fd4aa2f80d75ea749e12f9203c894abc
Kathleen Wang <kathleen@ou.edu>
2019-05-05 00:29:36 -0500

    options

---
+++ MVC_Project/MVC_Project/Controllers/OptionController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Project.Models;
+
+namespace MVC_Project.Controllers
+{
+    public class OptionController : Controller
+    {
+        // GET: Option
+        public ActionResult Index()
+        {
+            Option daily = new Option()
+            {
+                type = "Daily Ticket",
+                price = "64.99",
+                description = "",
+                img = @"daily.png"
+            };
+
+            Option season = new Option()
+            {
+                type = "Season Pass",
+                price = "79.99",
+                description = "",
+                img = @"season.png"
+            };
+
+            Option membership = new Option()
+            {
+                type = "Six Flags Membership",
+                price = "6.99 per month",
+                description = "",
+                img = @"membership.png"
+            };
+
+            List<Option> option = new List<Option>();
+            option.Add(daily);
+            option.Add(season);
+            option.Add(membership);
+
+            return View(option);
+        }
+    }
+}
---
+++ MVC_Project/MVC_Project/Models/Option.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Project.Models
+{
+    public class Option
+    {
+        public string type { get; set; }
+        public string price { get; set; }
+        public string description { get; set; }
+        public string img { get; set; }
+    }
+}
